071dd43eb8dfe4b541324ed6b7bd9b04587fe86d
Chris I'Anson <chris.ianson@digital.hmrc.gov.uk>
2020-03-19 08:52:11 +0000

    refactor test

--- test/models/GoodsItemSpec.scala
+++ test/models/GoodsItemSpec.scala
@@ -27,78 +27,7 @@ import scala.xml.Utility.trim
 
 class GoodsItemSpec extends FreeSpec with MustMatchers with Generators with ScalaCheckPropertyChecks {
 
-  private def packages(packages: Packages): Elem = {
-
-    val marksAndNumberPackage = packages.marksAndNumberPackage
-      .map {
-        marksAndNumber =>
-          <MarNumOfPacGS21>{marksAndNumber}</MarNumOfPacGS21>
-      }
-
-    val numberOfPackage = packages.numberOfPackages
-      .map {
-        number =>
-          <NumOfPacGS24>{number}</NumOfPacGS24>
-      }
-
-    val numberOfPieces = packages.numberOfPieces
-      .map {
-        number =>
-          <NumOfPieGS25>{number}</NumOfPieGS25>
-      }
-
-    {
-      <PACGS2>
-        {marksAndNumberPackage.getOrElse(NodeSeq.Empty)}
-        <KinOfPacGS23>
-        {packages.kindOfPackage}
-      </KinOfPacGS23>
-        {numberOfPackage.getOrElse(NodeSeq.Empty)}
-        {numberOfPieces.getOrElse(NodeSeq.Empty)}
-      </PACGS2>
-    }
-  }
-
-  private def producedDocument(goodsItem: GoodsItem): NonEmptyList[Elem] =
-    goodsItem.producedDocuments.map {
-
-      producedDocument =>
-        val reference = producedDocument.reference.map {
-          ref =>
-            <DocRefDC23>{ref}</DocRefDC23>
-        }
-
-        val complementOfInformation = producedDocument.complementOfInformation.map {
-          information =>
-            <ComOfInfDC25>{information}</ComOfInfDC25>
-        }
-
-        {
-          <PRODOCDC2>
-            <DocTypDC21>
-              {producedDocument.documentType}
-            </DocTypDC21>
-            {reference.getOrElse(false)}
-            {complementOfInformation.getOrElse(false)}
-          </PRODOCDC2>
-        }
-    }
-
-  private def sensitiveGoodsInformation(goodsItem: GoodsItem) =
-    goodsItem.sensitiveGoodsInformation.map {
-      sensitiveGoodsInformation =>
-        val goodsCode = sensitiveGoodsInformation.goodsCode.map {
-          code =>
-            <SenGooCodSD22>{code}</SenGooCodSD22>
-        }
-
-        <SGICODSD2>
-          {goodsCode.getOrElse(NodeSeq.Empty)}
-          <SenQuaSD23>
-            {sensitiveGoodsInformation.quantity}
-          </SenQuaSD23>
-        </SGICODSD2>
-    }.toList
+  import GoodsItemSpec._
 
   "GoodsItem" - {
 
@@ -168,7 +97,77 @@ class GoodsItemSpec extends FreeSpec with MustMatchers with Generators with Scal
   }
 }
 
-//ParseSuccess(GoodsItem(66,Some(쨷翄氷㶦ᄵ얹䜿㶉鵎ᒷ崮洗學㺖蕾恴漦릜Ꮱ⡿펥蝵勆⣎촌υ轆茀奠䌃蘩儤﬌닁胗㮥ि鳒簛伇Ὑ鵇牁盜샙큮鰢謆伴䵢ꩌ뵍뽶䍣䑢贐넌옮ﺫ꬐掼辿傠짓藾⨹뼓駻⑓᝻五␛栌੩❟쓼㱵穊鿒돲䴖秀啴펂呀選殘⢟媨遉纈Ồ),ɽ,Some(䓠튠秊馹ʢ綌噛䆃ぱ꼓㸺⇻쉼げꪚ꧜董柂놂),Some(뚂宜崜忤ꓦ䅪헉目嘻셡峦악汼긲㡬#㹋ꅷ嫪꬝㕲筜货ℨ脪＇糐谎汾닁ᤊᘰỠં抍旅숋坖ꂋ퍔샬阮螜︗뜬럏퉩뷕Ȕ),NonEmptyList(ProducedDocument(뮌,Some(☔앀㜃롥߯馫軘兩ז躞忻䥀뱫뻳朊㬶検稠곚쭆),Some(漆痑龅Ⱛ⪦꟞斍ﺑڋ죰ේ넆ﻃ䀇)), ProducedDocument(䃥,Some(뒪ⲭខ框涺ꂴ椀斬ᩒ鈀驣꾫ꇊ坕༫ꎯ坪鲢ￆ崷ː輇컉蜛渚៲쨲ᦳ),Some(ⷔ괓))),List(, ),Packages(Some(䐬睴),焣꫋,Some(10),Some(52)),
-// Some(Vector())))
-//ParseSuccess(GoodsItem(66,Some(쨷翄氷㶦ᄵ얹䜿㶉鵎ᒷ崮洗學㺖蕾恴漦릜Ꮱ⡿펥蝵勆⣎촌υ轆茀奠䌃蘩儤﬌닁胗㮥ि鳒簛伇Ὑ鵇牁盜샙큮鰢謆伴䵢ꩌ뵍뽶䍣䑢贐넌옮ﺫ꬐掼辿傠짓藾⨹뼓駻⑓᝻五␛栌੩❟쓼㱵穊鿒돲䴖秀啴펂呀選殘⢟媨遉纈Ồ),ɽ,Some(䓠튠秊馹ʢ綌噛䆃ぱ꼓㸺⇻쉼げꪚ꧜董柂놂),Some(뚂宜崜忤ꓦ䅪헉目嘻셡峦악汼긲㡬#㹋ꅷ嫪꬝㕲筜货ℨ脪＇糐谎汾닁ᤊᘰỠં抍旅숋坖ꂋ퍔샬阮螜︗뜬럏퉩뷕Ȕ),NonEmptyList(ProducedDocument(뮌,Some(☔앀㜃롥߯馫軘兩ז躞忻䥀뱫뻳朊㬶検稠곚쭆),Some(漆痑龅Ⱛ⪦꟞斍ﺑڋ죰ේ넆ﻃ䀇)), ProducedDocument(䃥,Some(뒪ⲭខ框涺ꂴ椀斬ᩒ鈀驣꾫ꇊ坕༫ꎯ坪鲢ￆ崷ː輇컉蜛渚៲쨲ᦳ),Some(ⷔ괓))),List(, ),Packages(Some(䐬睴),焣꫋,Some(10),Some(52)),
-// None))
+object GoodsItemSpec {
+  private def packages(packages: Packages): Elem = {
+
+    val marksAndNumberPackage = packages.marksAndNumberPackage
+      .map {
+        marksAndNumber =>
+          <MarNumOfPacGS21>{marksAndNumber}</MarNumOfPacGS21>
+      }
+
+    val numberOfPackage = packages.numberOfPackages
+      .map {
+        number =>
+          <NumOfPacGS24>{number}</NumOfPacGS24>
+      }
+
+    val numberOfPieces = packages.numberOfPieces
+      .map {
+        number =>
+          <NumOfPieGS25>{number}</NumOfPieGS25>
+      }
+
+    {
+      <PACGS2>
+        {marksAndNumberPackage.getOrElse(NodeSeq.Empty)}
+        <KinOfPacGS23>
+          {packages.kindOfPackage}
+        </KinOfPacGS23>
+        {numberOfPackage.getOrElse(NodeSeq.Empty)}
+        {numberOfPieces.getOrElse(NodeSeq.Empty)}
+      </PACGS2>
+    }
+  }
+
+  private def producedDocument(goodsItem: GoodsItem): NonEmptyList[Elem] =
+    goodsItem.producedDocuments.map {
+
+      producedDocument =>
+        val reference = producedDocument.reference.map {
+          ref =>
+            <DocRefDC23>{ref}</DocRefDC23>
+        }
+
+        val complementOfInformation = producedDocument.complementOfInformation.map {
+          information =>
+            <ComOfInfDC25>{information}</ComOfInfDC25>
+        }
+
+        {
+          <PRODOCDC2>
+          <DocTypDC21>
+            {producedDocument.documentType}
+          </DocTypDC21>
+          {reference.getOrElse(false)}
+          {complementOfInformation.getOrElse(false)}
+        </PRODOCDC2>
+        }
+    }
+
+  private def sensitiveGoodsInformation(goodsItem: GoodsItem) =
+    goodsItem.sensitiveGoodsInformation.map {
+      sensitiveGoodsInformation =>
+        val goodsCode = sensitiveGoodsInformation.goodsCode.map {
+          code =>
+            <SenGooCodSD22>{code}</SenGooCodSD22>
+        }
+
+        <SGICODSD2>
+          {goodsCode.getOrElse(NodeSeq.Empty)}
+          <SenQuaSD23>
+            {sensitiveGoodsInformation.quantity}
+          </SenQuaSD23>
+        </SGICODSD2>
+    }.toList
+}
