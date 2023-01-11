d77c50945d2a194f22f6036e9e383f8d55e69889
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2023-01-11 17:19:06 +0000

    CTCP-2230: Resolved bobby warning and updated other dependencies.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,8 +3,8 @@ import play.core.PlayVersion
 
 object AppDependencies {
 
-  private val mongoVersion = "0.73.0"
-  private val bootstrapVersion = "7.11.0"
+  private val mongoVersion = "0.74.0"
+  private val bootstrapVersion = "7.12.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
@@ -12,29 +12,28 @@ object AppDependencies {
     "uk.gov.hmrc"          %% "play-conditional-form-mapping"  % "1.12.0-play-28",
     "uk.gov.hmrc"          %% "bootstrap-frontend-play-28"     % bootstrapVersion,
     "uk.gov.hmrc"          %% "play-allowlist-filter"          % "1.1.0",
-    "uk.gov.hmrc"          %% "play-nunjucks"                  % "0.40.0-play-28",
-    "uk.gov.hmrc"          %% "play-nunjucks-viewmodel"        % "0.16.0-play-28",
-    "org.webjars.npm"      %  "govuk-frontend"                 % "4.3.1",
-    "uk.gov.hmrc.webjars"  %  "hmrc-frontend"                  % "5.11.2",
+    "uk.gov.hmrc"          %% "play-nunjucks"                  % "0.41.0-play-28",
+    "uk.gov.hmrc"          %% "play-nunjucks-viewmodel"        % "0.17.0-play-28",
+    "org.webjars.npm"      %  "govuk-frontend"                 % "4.4.1",
+    "uk.gov.hmrc.webjars"  %  "hmrc-frontend"                  % "5.19.0",
     "com.lucidchart"       %% "xtract"                         % "2.2.1"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo"         %% "hmrc-mongo-test-play-28"  % mongoVersion,
     "uk.gov.hmrc"               %% "bootstrap-test-play-28"   % bootstrapVersion,
-    "org.scalatest"             %% "scalatest"                % "3.2.14",
-    "org.scalatestplus.play"    %% "scalatestplus-play"       % "5.1.0",
-    "org.mockito"               %  "mockito-core"             % "4.8.0",
-    "org.scalatestplus"         %% "mockito-4-6"              % "3.2.14.0",
+    "org.scalatest"             %% "scalatest"                % "3.2.15",
+    "org.mockito"               %  "mockito-core"             % "4.11.0",
+    "org.scalatestplus"         %% "mockito-4-6"              % "3.2.15.0",
     "org.scalacheck"            %% "scalacheck"               % "1.17.0",
-    "org.scalatestplus"         %% "scalacheck-1-17"          % "3.2.14.0",
-    "io.github.wolfendale"      %% "scalacheck-gen-regexp"    % "1.0.0",
+    "org.scalatestplus"         %% "scalacheck-1-17"          % "3.2.15.0",
+    "io.github.wolfendale"      %% "scalacheck-gen-regexp"    % "1.1.0",
     "org.pegdown"               %  "pegdown"                  % "1.6.0",
     "org.jsoup"                 %  "jsoup"                    % "1.15.3",
     "com.typesafe.play"         %% "play-test"                % PlayVersion.current,
     "com.github.tomakehurst"    %  "wiremock-standalone"      % "2.27.2",
     "com.vladsch.flexmark"      %  "flexmark-all"             % "0.62.2"
-    ).map(_ % "test, it")
+  ).map(_ % "test, it")
 
   def apply(): Seq[ModuleID] = compile ++ test
 }
