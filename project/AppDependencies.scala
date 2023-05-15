4bca6017090d58f0e4d5425c54d33fc8c275a208
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2023-05-15 10:26:00 +0100

    Update AppDependencies.scala

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -13,9 +13,9 @@ object AppDependencies {
     "uk.gov.hmrc"          %% "bootstrap-frontend-play-28"     % bootstrapVersion,
     "uk.gov.hmrc"          %% "play-allowlist-filter"          % "1.1.0",
     "uk.gov.hmrc"          %% "play-nunjucks"                  % "0.41.0-play-28",
-    "uk.gov.hmrc"          %% "play-nunjucks-viewmodel"        % "0.17.0-play-28",
-    "org.webjars.npm"      %  "govuk-frontend"                 % "4.4.1",
-    "uk.gov.hmrc.webjars"  %  "hmrc-frontend"                  % "5.19.0",
+    "uk.gov.hmrc"          %% "play-nunjucks-viewmodel"        % "0.18.0-play-28",
+    "org.webjars.npm"      %  "govuk-frontend"                 % "4.6.0",
+    "uk.gov.hmrc.webjars"  %  "hmrc-frontend"                  % "5.31.0",
     "com.lucidchart"       %% "xtract"                         % "2.2.1"
   )
 
